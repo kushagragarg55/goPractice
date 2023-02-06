25e3d2467e796f4947e7a28eab5cea1f67292f7d
Kushagra Garg <gakushag@amazon.com>
2023-02-06 17:38:34 +0530

    go practice day3

--- Strings-and-runes.go
+++ Strings-and-runes.go
@@ -1,3 +1,7 @@
 package main
 
-func
+import "fmt"
+
+func main(){
+
+}
--- pointers.go
+++ pointers.go
@@ -16,7 +16,8 @@ func main(){
     fmt.Println("initial value of i")
     zeroval(i)
     fmt.Println("Value of i after calling zeroval function: ", i)
-
+    fmt.Println("value of i address of i ",&i)
     zeroptr(&i)
     fmt.Println("Value of i after calling zeroptr function: ", i)
+    fmt.Println("value of address of i after calling zeroptr function",&i)
 }
