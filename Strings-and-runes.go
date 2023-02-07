ce72151e4fa47a5b007e2f32014dec114290137a
Kushagra Garg <gakushag@amazon.com>
2023-02-07 13:57:11 +0530

    go practice day4

--- Strings-and-runes.go
+++ Strings-and-runes.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func main(){
+    const s = "สวัสดี"
+
+    fmt.Println("Len: ", len(s))
+
+    for i:=0;i<len(s);i++{
+        fmt.Printf("%x ",s[i])
+    }
+    fmt.Println()
+
 
 }
